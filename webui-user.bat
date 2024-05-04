b102e774603a71e0e51534186ead289555098702
Kyle Scott <kylescott@rawintelligence.org>
2024-05-04 18:57:46 -0500

    changes to extension adds

--- webui-user.bat
+++ webui-user.bat
@@ -2,7 +2,14 @@
 
 set PYTHON=
 set GIT=
-set VENV_DIR=
-set COMMANDLINE_ARGS=
+set VENV_DIR= venv/Scripts
+COMMANDLINE_ARGS= --xformers  --ckpt-dir D:\\checkpoints
+rem set COMMANDLINE_ARGS=--allow-code --xformers --api --ckpt-dir D:\\checkpoints --listen
+rem COMMANDLINE_ARGS=--ckpt-dir D:\\checkpoints --listen --api
+rem set COMMANDLINE_ARGS=--ckpt-dir D:\\checkpoints
+rem set COMMANDLINE_ARGS= --ckpt-dir D:\\checkpoint
+rem COMMANDLINE_ARGS= --ckpt-dir D:\\checkpoint --listen --api
+rem set COMMANDLINE_ARGS= --ckpt-dir D:\\checkpoint
+rem COMMANDLINE_ARGS= --ckpt-dir D:\\checkpoint --listen --api
 
 call webui.bat
